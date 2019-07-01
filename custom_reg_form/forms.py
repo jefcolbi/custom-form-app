472ee8704667a982fd20c97dd74f8c9ef914cc8f
jefcolbi <jefcolbi@gmail.com>
2019-07-02 00:33:14 +0100

    Update forms.py

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -33,7 +33,7 @@ class ExtraInfoForm(ModelForm):
     The fields on this form are derived from the ExtraInfo model in models.py.
     """
     
-    number = forms.CharField(validators=[validate_number])
+    number = forms.CharField(validators=[validate_number], help_text="Phone number")
         
     def __init__(self, *args, **kwargs):
         super(ExtraInfoForm, self).__init__(*args, **kwargs)
