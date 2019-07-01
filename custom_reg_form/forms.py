33a92286af62928891343a9fd1ff6af3dc5a331b
jefcolbi <jefcolbi@gmail.com>
2019-07-01 20:12:53 +0100

    Update forms.py

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -1,5 +1,32 @@
 from .models import ExtraInfo
+from django import forms
 from django.forms import ModelForm
+import re
+
+RGX_NUMBER = re.compile(r"(?:\+234)?(?:070|080|081|090)\d{8}")
+
+def validate_number(value):
+    if not RGX_NUMBER.match(value):
+        raise forms.ValidationError(_('Please enter a valid phone number'))
+
+class NigerianField(forms.CharField):
+    """
+    A CharField that allows only nigerian phone numbers
+    """
+
+    default_validators = [validate_number]
+
+    def __init__(self, *args, **kwargs):
+        super(NigerianField, self).__init__(
+            min_length=11,
+            max_length=15,
+            error_messages={
+                "required": "You need to set this number field",
+                "min_length": "Your phone number is too short",
+                "max_length": "Your phone number is too big",
+            }
+        )
+
 
 class ExtraInfoForm(ModelForm):
     """
@@ -7,11 +34,13 @@ class ExtraInfoForm(ModelForm):
     """
     def __init__(self, *args, **kwargs):
         super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields['favorite_movie'].error_messages = {
-            "required": u"Please tell us your favorite movie.",
-            "invalid": u"We're pretty sure you made that movie up.",
+        self.fields['number'].error_messages = {
+            "required": u"Please give us your number.",
+            "invalid": u"Please use a valid nigerian phone number.",
         }
+        
 
     class Meta(object):
         model = ExtraInfo
-        fields = ('favorite_editor', 'favorite_movie')
+        fields = ('number',)
+        widgets = {'number': NigerianField()}
