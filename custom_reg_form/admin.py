ab3a97ffc217704b1d804afa5d39b940fc5df596
jefcolbi <jefcolbi@gmail.com>
2019-07-02 01:26:55 +0100

    Update admin.py

--- custom_reg_form/admin.py
+++ custom_reg_form/admin.py
@@ -4,4 +4,4 @@ from .models import ExtraInfo
 class ExtraInfoAdmin(admin.ModelAdmin):
     list_display = ('user', 'number')
 
-admin.site.register(ExtraInfo)
+admin.site.register(ExtraInfo, ExtraInfoAdmin)
